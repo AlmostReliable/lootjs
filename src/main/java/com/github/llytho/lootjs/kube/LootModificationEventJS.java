eced7f5e6d1469348b33967744863091465c29e5
Erik <29131229+LLytho@users.noreply.github.com>
2021-12-12 15:12:59 +0100

    Add `removeGlobalLoot` to remove global modifiers from other mods

--- src/main/java/com/github/llytho/lootjs/kube/LootModificationEventJS.java
+++ src/main/java/com/github/llytho/lootjs/kube/LootModificationEventJS.java
@@ -4,14 +4,48 @@ import com.github.llytho.lootjs.LootModificationsAPI;
 import com.github.llytho.lootjs.action.CompositeAction;
 import dev.latvian.kubejs.event.EventJS;
 import dev.latvian.mods.rhino.util.HideFromJS;
+import net.minecraft.util.ResourceLocation;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class LootModificationEventJS extends EventJS {
 
     private final List<CompositeLootActionBuilder> modifierBuilders = new ArrayList<>();
+    private final List<ResourceLocation> originalLocations;
+    private final Set<ResourceLocation> locationsToRemove = new HashSet<>();
+
+    public LootModificationEventJS(ArrayList<ResourceLocation> originalLocations) {
+        this.originalLocations = originalLocations;
+    }
+
+    public List<ResourceLocation> getModifiers() {
+        return Collections.unmodifiableList(originalLocations);
+    }
+
+    public void removeGlobalLoot(String... locationOrModIds) {
+        Set<String> modIds = new HashSet<>();
+        Set<ResourceLocation> locations = new HashSet<>();
+        for (String locationOrModId : locationOrModIds) {
+            if (locationOrModId.startsWith("@")) {
+                modIds.add(locationOrModId.substring(1));
+            } else {
+                locations.add(new ResourceLocation(locationOrModId));
+            }
+        }
+
+        Set<ResourceLocation> collectedByModIds = originalLocations
+                .stream()
+                .filter(rl -> modIds.contains(rl.getNamespace()))
+                .collect(Collectors.toSet());
+        Set<ResourceLocation> collectedByLocations = originalLocations
+                .stream()
+                .filter(locations::contains)
+                .collect(Collectors.toSet());
+
+        locationsToRemove.addAll(collectedByModIds);
+        locationsToRemove.addAll(collectedByLocations);
+    }
 
     public CompositeLootActionBuilder addModifier() {
         CompositeLootActionBuilder builder = new CompositeLootActionBuilder();
@@ -36,5 +70,7 @@ public class LootModificationEventJS extends EventJS {
         for (CompositeAction action : actions) {
             LootModificationsAPI.get().addAction(action);
         }
+
+        originalLocations.removeIf(locationsToRemove::contains);
     }
 }
--- src/main/java/com/github/llytho/lootjs/mixin/LootModifierManagerMixin.java
+++ src/main/java/com/github/llytho/lootjs/mixin/LootModifierManagerMixin.java
@@ -10,6 +10,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 import java.util.ArrayList;
+import java.util.Set;
 import java.util.function.Consumer;
 
 @Mixin(LootModifierManager.class)
@@ -18,7 +19,7 @@ public class LootModifierManagerMixin {
     @Redirect(method = "apply", at = @At(value = "INVOKE", target = "Ljava/util/ArrayList;forEach(Ljava/util/function/Consumer;)V", ordinal = 0), remap = false)
     private void lootModifierReload(ArrayList<ResourceLocation> locations, Consumer<ResourceLocation> originalAction) {
         LootModificationsAPI.reload();
-        new LootModificationEventJS().post(ScriptType.SERVER, "global_loot_modification");
+        new LootModificationEventJS(locations).post(ScriptType.SERVER, "global_loot_modification");
         locations.forEach(originalAction);
     }
 }
