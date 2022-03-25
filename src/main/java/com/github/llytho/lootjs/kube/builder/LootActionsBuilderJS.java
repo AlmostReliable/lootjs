a63fb8681693f090783bdb5a1349f8f909d3bcc7
LLytho <29131229+LLytho@users.noreply.github.com>
2022-03-25 14:00:26 +0100

    Update LootActionsBuilderJS.java

--- src/main/java/com/github/llytho/lootjs/kube/builder/LootActionsBuilderJS.java
+++ src/main/java/com/github/llytho/lootjs/kube/builder/LootActionsBuilderJS.java
@@ -10,6 +10,7 @@ import com.github.llytho.lootjs.util.Utils;
 import com.github.llytho.lootjs.util.WeightedItemStack;
 import dev.latvian.mods.kubejs.item.ItemStackJS;
 import dev.latvian.mods.kubejs.item.ingredient.IngredientJS;
+import dev.latvian.mods.kubejs.util.ConsoleJS;
 import dev.latvian.mods.rhino.util.HideFromJS;
 import net.minecraft.advancements.critereon.MinMaxBounds;
 import net.minecraft.util.random.SimpleWeightedRandomList;
@@ -31,18 +32,21 @@ public class LootActionsBuilderJS implements ConditionsContainer<LootActionsBuil
 
     @Deprecated(forRemoval = true, since = DEPRECATED_MSG + "name(name)")
     public LootActionsBuilderJS logName(String name) {
+        ConsoleJS.SERVER.warn(DEPRECATED_MSG + "name(name)");
         logName = name;
         return this;
     }
 
     @Deprecated(forRemoval = true, since = DEPRECATED_MSG + "apply((ctx) => {})")
     public LootActionsBuilderJS thenApply(Consumer<LootContextJS> action) {
+        ConsoleJS.SERVER.warn(DEPRECATED_MSG + "apply((ctx) => {})");
         handlers.add(new CustomJSAction(action));
         return this;
     }
 
     @Deprecated(forRemoval = true, since = DEPRECATED_MSG + "addLoot([items])")
     public LootActionsBuilderJS thenAdd(ItemStackJS... itemStacks) {
+        ConsoleJS.SERVER.warn(DEPRECATED_MSG + "addLoot([items])");
         ItemStack[] vanillaItemStacks = Arrays
                 .stream(itemStacks)
                 .filter(is -> !is.isEmpty())
@@ -55,6 +59,7 @@ public class LootActionsBuilderJS implements ConditionsContainer<LootActionsBuil
 
     @Deprecated(forRemoval = true, since = DEPRECATED_MSG + "addWeightedLoot(interval, allowDuplicateLoot, [items])")
     public LootActionsBuilderJS thenAddWeighted(MinMaxBounds.Ints interval, boolean allowDuplicateLoot, WeightedItemStack[] itemStacks) {
+        ConsoleJS.SERVER.warn(DEPRECATED_MSG + "addWeightedLoot(interval, allowDuplicateLoot, [items])");
         var weightedListBuilder = SimpleWeightedRandomList.<ItemStack>builder();
         for (var wis : itemStacks) {
             weightedListBuilder.add(wis.getItemStack(), wis.getWeight());
@@ -65,17 +70,20 @@ public class LootActionsBuilderJS implements ConditionsContainer<LootActionsBuil
 
     @Deprecated(forRemoval = true, since = DEPRECATED_MSG + "addWeightedLoot(interval, [items])")
     public LootActionsBuilderJS thenAddWeighted(MinMaxBounds.Ints interval, WeightedItemStack[] itemStacks) {
+        ConsoleJS.SERVER.warn(DEPRECATED_MSG + "addWeightedLoot(interval, [items])");
         return thenAddWeighted(interval, true, itemStacks);
     }
 
     @Deprecated(forRemoval = true, since = DEPRECATED_MSG + "addWeightedLoot([items])")
     public LootActionsBuilderJS thenAddWeighted(WeightedItemStack[] itemStacks) {
+        ConsoleJS.SERVER.warn(DEPRECATED_MSG + "addWeightedLoot([items])");
         return thenAddWeighted(MinMaxBounds.Ints.atMost(1), true, itemStacks);
     }
 
 
     @Deprecated(forRemoval = true, since = DEPRECATED_MSG + "removeLoot(ingredient)")
     public LootActionsBuilderJS thenRemove(IngredientJS ingredient) {
+        ConsoleJS.SERVER.warn(DEPRECATED_MSG + "removeLoot(ingredient)");
         handlers.add(new RemoveLootAction(ingredient.getVanillaPredicate()));
         return this;
     }
@@ -83,12 +91,14 @@ public class LootActionsBuilderJS implements ConditionsContainer<LootActionsBuil
 
     @Deprecated(forRemoval = true, since = DEPRECATED_MSG + "replaceLoot(ingredient, item)")
     public LootActionsBuilderJS thenReplace(IngredientJS ingredient, ItemStackJS itemStack) {
+        ConsoleJS.SERVER.warn(DEPRECATED_MSG + "replaceLoot(ingredient, item)");
         handlers.add(new ReplaceLootAction(ingredient.getVanillaPredicate(), itemStack.getItemStack()));
         return this;
     }
 
     @Deprecated(forRemoval = true, since = DEPRECATED_MSG + "modifyLoot(ingredient, (item) => { return item; })")
     public LootActionsBuilderJS thenModify(IngredientJS ingredient, Function<ItemStackJS, ItemStackJS> function) {
+        ConsoleJS.SERVER.warn(DEPRECATED_MSG + "modifyLoot(ingredient, (item) => { return item; })");
         handlers.add(new ModifyLootAction(ingredient.getVanillaPredicate(),
                 (itemStack) -> function.apply(new ItemStackJS(itemStack)).getItemStack()));
         return this;
@@ -96,6 +106,7 @@ public class LootActionsBuilderJS implements ConditionsContainer<LootActionsBuil
 
     @Deprecated(forRemoval = true, since = DEPRECATED_MSG + "triggerExplosion(radius, destroy, fire)")
     public LootActionsBuilderJS thenExplode(float radius, boolean destroy, boolean fire) {
+        ConsoleJS.SERVER.warn(DEPRECATED_MSG + "triggerExplosion(radius, destroy, fire)");
         Explosion.BlockInteraction mode =
                 destroy ? Explosion.BlockInteraction.DESTROY : Explosion.BlockInteraction.NONE;
         handlers.add(new ExplodeAction(radius, mode, fire));
@@ -104,17 +115,20 @@ public class LootActionsBuilderJS implements ConditionsContainer<LootActionsBuil
 
     @Deprecated(forRemoval = true, since = DEPRECATED_MSG + "triggerLightningStrike(shouldDamage)")
     public LootActionsBuilderJS thenLightningStrike(boolean shouldDamage) {
+        ConsoleJS.SERVER.warn(DEPRECATED_MSG + "triggerLightningStrike(shouldDamage)");
         handlers.add(new LightningStrikeAction(shouldDamage));
         return this;
     }
 
     @Deprecated(forRemoval = true, since = DEPRECATED_MSG + "pool((pool) => {})")
     public LootActionsBuilderJS addPool(Consumer<LootActionsBuilderJS> callback) {
+        ConsoleJS.SERVER.warn(DEPRECATED_MSG + "pool((pool) => {})");
         return thenRollPool(MinMaxBounds.Ints.exactly(1), callback);
     }
 
     @Deprecated(forRemoval = true, since = DEPRECATED_MSG + "pool(interval, (pool) => {})")
     public LootActionsBuilderJS thenRollPool(MinMaxBounds.Ints interval, Consumer<LootActionsBuilderJS> callback) {
+        ConsoleJS.SERVER.warn(DEPRECATED_MSG + "pool(interval, (pool) => {})");
         LootActionsBuilderJS poolBuilder = new LootActionsBuilderJS();
         callback.accept(poolBuilder);
         List<ILootHandler> poolHandlers = poolBuilder.getHandlers();
