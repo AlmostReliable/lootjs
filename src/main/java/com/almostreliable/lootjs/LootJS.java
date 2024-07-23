a71a07646c99962a3c7a00339f9528e8e5505be5
LLytho <main@lytho.dev>
2024-07-23 18:04:47 +0200

    Bump kubejs and neoforge

--- src/main/java/com/almostreliable/lootjs/LootJS.java
+++ src/main/java/com/almostreliable/lootjs/LootJS.java
@@ -1,8 +1,6 @@
 package com.almostreliable.lootjs;
 
 import com.almostreliable.lootjs.core.filters.ItemFilterWrapper;
-import com.almostreliable.lootjs.util.ModHolderSet;
-import net.minecraft.core.Holder;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.Registry;
 import net.minecraft.core.RegistryAccess;
@@ -12,10 +10,7 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.neoforge.registries.DeferredRegister;
-import net.neoforged.neoforge.registries.NeoForgeRegistries;
 import net.neoforged.neoforge.registries.RegisterEvent;
-import net.neoforged.neoforge.registries.holdersets.HolderSetType;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -28,10 +23,6 @@ public class LootJS {
     public static final Logger LOG = LogManager.getLogger("LootJS");
     public static Consumer<String> DEBUG_ACTION = LOG::info;
 
-    private static final DeferredRegister<HolderSetType> HOLDER_SET_TYPES = DeferredRegister.create(NeoForgeRegistries.Keys.HOLDER_SET_TYPES,
-            BuildConfig.MOD_ID);
-    public static final Holder<HolderSetType> MOD_HOLDER_SET = HOLDER_SET_TYPES.register("by_mod",
-            () -> ModHolderSet::codec);
     public static HolderLookup.Provider LOOKUP_PROVIDER = new HolderLookup.Provider() {
         @Override
         public Stream<ResourceKey<? extends Registry<?>>> listRegistries() {
@@ -46,7 +37,6 @@ public class LootJS {
 
     public LootJS(IEventBus bus) {
         bus.addListener(this::onRegister);
-        HOLDER_SET_TYPES.register(bus);
         LootJSConditions.CONDITIONS.register(bus);
     }
 
