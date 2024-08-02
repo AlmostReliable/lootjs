adffc5dd4361e9b8f310fbd3dbd36ed70e0381b6
LLytho <main@lytho.dev>
2024-08-02 22:58:34 +0200

    Remove `addItem` in loot appender

--- src/main/java/com/almostreliable/lootjs/loot/table/LootEntryAppender.java
+++ src/main/java/com/almostreliable/lootjs/loot/table/LootEntryAppender.java
@@ -3,17 +3,12 @@ package com.almostreliable.lootjs.loot.table;
 import com.almostreliable.lootjs.core.entry.LootEntry;
 import com.google.gson.JsonObject;
 import com.mojang.serialization.JsonOps;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.storage.loot.entries.LootPoolEntries;
 
 public interface LootEntryAppender {
     default LootEntryAppender addCustomEntry(JsonObject json) {
         var vanilla = LootPoolEntries.CODEC.parse(JsonOps.INSTANCE, json).getOrThrow();
         return addEntry(LootEntry.ofVanilla(vanilla));
-    }
-
-    default LootEntryAppender addItem(ItemStack itemStack) {
-        return addEntry(LootEntry.of(itemStack));
     }
 
     LootEntryAppender addEntry(LootEntry entry);
